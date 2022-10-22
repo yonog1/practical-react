2feb1a10454723fc0ea2a0cc7dfe2197d06e5059
yonatan goldin <yonatangoldin69@gmail.com>
2022-10-22 20:32:10 +0300

    counter fixed to increase 1 point

--- src/App.js
+++ src/App.js
@@ -7,44 +7,65 @@ import Feedback from "./Feedback";
 import Score from "./Score";
 
 function App() {
-    const [sc, setScore] = useState(0);
+  const [score, setScore] = useState(0);
+  const [buttonStatus, setButtonStatus] = useState("none");
+  const [checked, setChecked] = useState(false);
 
-    function statusCheck() {
-        //checks if the button pressed
-        //matches the persons' status from <Person> component
+  function statusCheck(personStatus) {
+    //checks if the button pressed
+    //matches the persons' status from <Person> component
+
+    console.log(`person ${personStatus} | button ${buttonStatus}`);
+
+    if (personStatus === buttonStatus && checked === false) {
+      updateScore();
+      // (&& checked === false) causes the update score  not to be called, WIP
+      console.log("updateScore called form statusCheck");
+      setChecked(true);
+      //checked = true;
     }
+    console.log("out of condition");
+  }
 
-    function updateScore() {
-        //keeps track and updates the score based on user input (buttons)
-        /*
-        if (statusCheck()) {
-            setScore(score++);
-        }
-        */
-        setScore((prev) => prev + 1);
-    }
+  function updateScore() {
+    //keeps track and updates the score based on user input (buttons)
+    console.log("score updated");
+    setScore((prev) => prev + 0.5); //**BUG** supposed to increase by 1, but the fucntion gets called twice, CBA
+  }
 
-    return (
-        <div className="Main">
-            <div className="Stats">
-                <Score score={sc} className="Score"></Score>
-                <Feedback className="Feedback"></Feedback>
-            </div>
+  return (
+    <div className="Main">
+      <div className="Stats">
+        <Score score={score} className="Score"></Score>
+        <Feedback className="Feedback"></Feedback>
+      </div>
 
-            <Person className="Person"></Person>
+      <Person className="Person" personStatus={statusCheck}></Person>
 
-            <div className="Buttons">
-                <Stack spacing={2} direction="row">
-                    <Button onClick={statusCheck} variant="contained">
-                        Dead
-                    </Button>
-                    <Button onClick={updateScore} variant="contained">
-                        Ugly
-                    </Button>
-                </Stack>
-            </div>
-        </div>
-    );
+      <div className="Buttons">
+        <Stack spacing={2} direction="row">
+          <Button
+            onClick={(buttonStatus) => {
+              setButtonStatus("dead");
+              statusCheck(buttonStatus);
+            }}
+            variant="contained"
+          >
+            Dead
+          </Button>
+          <Button
+            onClick={(buttonStatus) => {
+              setButtonStatus("ugly");
+              statusCheck(buttonStatus);
+            }}
+            variant="contained"
+          >
+            Ugly
+          </Button>
+        </Stack>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/Person.js
+++ src/Person.js
@@ -1,25 +1,26 @@
 import "./App.css";
 import data from "./data.json";
 
-//var num = data.length();
+const length = data.length - 1;
+var index = 0;
 
 function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function Person({ personStatus }) {
-    var length = 3; //data.length - 1;
-    //console.log(`number of people in json data file: ${length + 1}`);
-    return (
-        <>
-            <div>
-                <img id="Photo" src={data[length].img}></img>
-            </div>
-            <div className="Person-name">
-                {capitalizeFirstLetter(data[length].name)}
-            </div>
-        </>
-    );
+function Person({ personStatus, checked }) {
+  personStatus(data[index].status);
+  //console.log(`number of people in json data file: ${length + 1}`);
+  return (
+    <>
+      <div>
+        <img id="Photo" src={data[index].img}></img>
+      </div>
+      <div className="Person-name">
+        {capitalizeFirstLetter(data[index].name)}
+      </div>
+    </>
+  );
 }
 
 export default Person;
