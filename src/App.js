19012e29395a13e00e71b04d58bd028bd2923212
yonog1 <yonatangoldin69@gmail.com>
2022-11-04 07:31:43 +0200

    status check flawed, in debugging process

--- src/App.js
+++ src/App.js
@@ -9,99 +9,85 @@ import Score from "./Score";
 import data from "./data.json";
 
 function App() {
-  const [score, setScore] = useState(0);
-  const [buttonStatus, setButtonStatus] = useState("none");
-  const [checked, setChecked] = useState(false);
+    const [score, setScore] = useState(0);
+    const [buttonStatus, setButtonStatus] = useState("none");
+    const [checked, setChecked] = useState(false);
 
-  const [index, setIndex] = useState(0);
+    const [index, setIndex] = useState(0);
 
-  function statusCheck(personStatus) {
-    //checks if the button pressed
-    //matches the persons' status from <Person> component
+    function statusCheck(personStatus) {
+        //checks if the button pressed
+        //matches the persons' status from <Person> component
 
-    console.log(`person ${personStatus} | button ${buttonStatus}`);
+        console.log(`person ${personStatus} | button ${buttonStatus}`);
 
-    console.log("start of condition");
-    console.log(
-      "pesron status: ",
-      personStatus,
-      " | ",
-      "checked: ",
-      checked === false
-    );
+        if (checked === false && personStatus === buttonStatus) {
+            //incrementing score incorrectly, opposing status' are compared
 
-    if (checked === false && personStatus === buttonStatus) {
-      //incrementing score incorrectly, opposing status' are compared
-      console.log(
-        "checked condition",
-        personStatus === buttonStatus,
-        checked === false
-      );
-      updateScore();
+            updateScore();
 
-      console.log("updateScore called form statusCheck");
-      setChecked(true);
+            console.log("updateScore called form statusCheck");
+            setChecked(true);
+        }
     }
-    console.log("end of condition");
-  }
 
-  function updateScore() {
-    //keeps track and updates the score based on user input (buttons)
-    console.log("score updated");
-    setScore((prev) => prev + 0.5); //**BUG** supposed to increase by 1, but the fucntion gets called twice, CBA
-  }
+    function updateScore() {
+        //keeps track and updates the score based on user input (buttons)
+        console.log("score updated");
+        setScore((prev) => prev + 0.5); //**BUG** supposed to increase by 1, but the fucntion gets called twice, CBA
+    }
 
-  return (
-    <div className="Main">
-      <div className="Stats">
-        <Score score={score} className="Score"></Score>
-      </div>
+    return (
+        <div className="Main">
+            <div className="Stats">
+                <Score score={score} className="Score"></Score>
+            </div>
 
-      <Person
-        className="Person"
-        personStatus={statusCheck}
-        index={index}
-        setIndex={setIndex}
-      ></Person>
+            <Person
+                className="Person"
+                personStatus={statusCheck}
+                index={index}
+                setIndex={setIndex}
+            ></Person>
 
-      <div className="Buttons">
-        <Stack spacing={2} direction="row">
-          <Button
-            onClick={(buttonStatus) => {
-              setChecked(false);
-              console.log(index);
-              if (index < data.length - 1) {
-                console.log(index);
-                setIndex((prev) => prev + 1);
-                console.log(index);
-              }
-              setButtonStatus("dead");
-              statusCheck(buttonStatus);
-            }}
-            variant="contained"
-          >
-            Dead
-          </Button>
-          <Button
-            onClick={(buttonStatus) => {
-              setChecked(false);
-              console.log(index);
-              if (index < data.length - 1) {
-                console.log(index);
-                setIndex((prev) => prev + 1);
-                console.log(index);
-              }
-              setButtonStatus("ugly");
-              statusCheck(buttonStatus);
-            }}
-            variant="contained"
-          >
-            Ugly
-          </Button>
-        </Stack>
-      </div>
-    </div>
-  );
+            <div className="Buttons">
+                <Stack spacing={2} direction="row">
+                    <Button
+                        onClick={(buttonStatus) => {
+                            setChecked(false);
+                            setButtonStatus("dead");
+
+                            if (index < data.length - 1) {
+                                console.log(index);
+                                setIndex((prev) => prev + 1);
+
+                                statusCheck(buttonStatus);
+                            }
+                        }}
+                        variant="contained"
+                    >
+                        Dead
+                    </Button>
+                    <Button
+                        onClick={(buttonStatus) => {
+                            setChecked(false);
+                            setButtonStatus("ugly");
+
+                            if (index < data.length - 1) {
+                                console.log(index);
+                                setIndex((prev) => prev + 1);
+
+                                statusCheck(buttonStatus);
+                            }
+                        }}
+                        variant="contained"
+                    >
+                        Ugly
+                    </Button>
+                </Stack>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- src/Person.js
+++ src/Person.js
@@ -1,24 +1,27 @@
-import { useState } from "react";
 import "./App.css";
 import data from "./data.json";
 
 function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
+    return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 function Person(props) {
-  props.personStatus(data[props.index].status);
+    props.personStatus(data[props.index].status);
 
-  return (
-    <>
-      <div>
-        <img id="Photo" src={data[props.index].img}></img>
-      </div>
-      <div className="Person-name">
-        {capitalizeFirstLetter(data[props.index].name)}
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div>
+                <img
+                    id="Photo"
+                    src={data[props.index].img}
+                    alt="preson's' face"
+                ></img>
+            </div>
+            <div className="Person-name">
+                {capitalizeFirstLetter(data[props.index].name)}
+            </div>
+        </>
+    );
 }
 
 export default Person;
